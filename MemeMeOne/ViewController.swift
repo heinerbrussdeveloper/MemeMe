c7d8d63f6eec60e04f0aa4f319ebd198b63c303e
Heiner Bruss <heiner.bruss@gmail.com>
2020-02-09 19:03:22 +0100

    Initial Commit

---
+++ MemeMeOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMeOne
+//
+//  Created by Heiner Bruß on 09.02.20.
+//  Copyright © 2020 Heiner Bruß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
